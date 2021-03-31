0c271f9307c51a01fc82c278009b52af08777b60
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-31 14:30:08 +0700

    Create a new project

---
+++ spring-boot-journal/src/main/java/io/erikrios/github/springbootjournal/SpringBootJournalApplication.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.springbootjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJournalApplication.class, args);
+	}
+
+}
---
+++ spring-boot-journal/src/test/java/io/erikrios/github/springbootjournal/SpringBootJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.springbootjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
