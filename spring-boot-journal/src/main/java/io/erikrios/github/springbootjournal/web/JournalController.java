bd009c179d55dbab968a4fd4146440f582f34a08
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-01 00:34:56 +0700

    Add an endpoint to get all journal

--- spring-boot-journal/src/main/java/io/erikrios/github/springbootjournal/web/JournalController.java
+++ spring-boot-journal/src/main/java/io/erikrios/github/springbootjournal/web/JournalController.java
@@ -1,21 +1,31 @@
 package io.erikrios.github.springbootjournal.web;
 
+import io.erikrios.github.springbootjournal.domain.Journal;
+import io.erikrios.github.springbootjournal.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import io.erikrios.github.springbootjournal.repository.JournalRepository;
+import java.util.List;
 
 @Controller
 public class JournalController {
 
-	@Autowired
-	JournalRepository repository;
+    @Autowired
+    JournalRepository repository;
 
-	@RequestMapping("/")
-	public String index(Model model) {
-		model.addAttribute("journal", repository.findAll());
-		return "index";
-	}
+    @RequestMapping(value = "/journal", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public @ResponseBody
+    List<Journal> getJournal() {
+        return repository.findAll();
+    }
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("journal", repository.findAll());
+        return "index";
+    }
 }
