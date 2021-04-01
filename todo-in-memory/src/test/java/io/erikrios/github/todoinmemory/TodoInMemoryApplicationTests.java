cce4a53462d9a44a7e3efadb9125aac72c6f9a02
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-01 20:54:06 +0700

    Add a new project

---
+++ todo-in-memory/src/test/java/io/erikrios/github/todoinmemory/TodoInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.todoinmemory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
