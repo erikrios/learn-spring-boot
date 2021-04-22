232b1d0b538261bc59ae88f666a119f5720fc1ce
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-22 19:04:49 +0700

    Create a new project

---
+++ soap-web-services/src/test/java/io/erikrio/github/webservices/SoapWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erikrio.github.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
