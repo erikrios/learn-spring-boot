d4f49c25a3ec0b8978d380bcb4879379acc379c0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-04-23 19:13:05 +0700

    Refactor the code

--- soap-web-services/src/main/java/io/erikrios/github/webservices/SoapWebServicesApplication.java
+++ soap-web-services/src/main/java/io/erikrios/github/webservices/SoapWebServicesApplication.java
@@ -1,4 +1,4 @@
-package io.erikrio.github.webservices;
+package io.erikrios.github.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- soap-web-services/src/test/java/io/erikrios/github/webservices/SoapWebServicesApplicationTests.java
+++ soap-web-services/src/test/java/io/erikrios/github/webservices/SoapWebServicesApplicationTests.java
@@ -1,4 +1,4 @@
-package io.erikrio.github.webservices;
+package io.erikrios.github.webservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
