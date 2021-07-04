56142021c5d7c38c193f82e946044d5217dfc1eb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-04 20:29:52 +0700

    Add @EnableScheduling annotation

--- scheduling-tasks/src/main/java/io/erikrios/github/schedulingtasks/SchedulingTasksApplication.java
+++ scheduling-tasks/src/main/java/io/erikrios/github/schedulingtasks/SchedulingTasksApplication.java
@@ -2,8 +2,10 @@ package io.erikrios.github.schedulingtasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingTasksApplication {
 
     public static void main(String[] args) {
