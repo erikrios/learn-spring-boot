d51ecc233354465574a4d3efa23a950acd2b5088
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-04 18:54:47 +0700

    Create a new project

---
+++ building-a-restful-web-service/src/test/java/erikrios/github/io/buildingarestfulwebservice/BuildingARestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package erikrios.github.io.buildingarestfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingARestfulWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
