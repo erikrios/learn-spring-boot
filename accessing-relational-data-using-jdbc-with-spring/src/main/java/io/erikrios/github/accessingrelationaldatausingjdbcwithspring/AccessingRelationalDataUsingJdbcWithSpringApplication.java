7df12fae25e4941e136fa04959c71769c37d2630
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-04 21:03:28 +0700

    Create a new project

---
+++ accessing-relational-data-using-jdbc-with-spring/src/main/java/io/erikrios/github/accessingrelationaldatausingjdbcwithspring/AccessingRelationalDataUsingJdbcWithSpringApplication.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.accessingrelationaldatausingjdbcwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingRelationalDataUsingJdbcWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingRelationalDataUsingJdbcWithSpringApplication.class, args);
+    }
+
+}
---
+++ accessing-relational-data-using-jdbc-with-spring/src/test/java/io/erikrios/github/accessingrelationaldatausingjdbcwithspring/AccessingRelationalDataUsingJdbcWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.accessingrelationaldatausingjdbcwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingRelationalDataUsingJdbcWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
