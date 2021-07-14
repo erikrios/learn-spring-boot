e0b3b4de6d22c6fb46e6d308bda987421cd4ad1d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-14 19:08:08 +0700

    Create a new project

---
+++ my-note/src/test/java/io/erikrios/github/mynote/MyNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.erikrios.github.mynote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
