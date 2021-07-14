455fe6c593b7915403e0f17efa7ab3aa5b57bc95
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-14 19:26:20 +0700

    Load the greeting message from properties file

--- my-note/src/main/java/io/erikrios/github/mynote/controller/HomeController.java
+++ my-note/src/main/java/io/erikrios/github/mynote/controller/HomeController.java
@@ -1,14 +1,18 @@
 package io.erikrios.github.mynote.controller;
 
 import io.erikrios.github.mynote.domain.Response;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
     @GetMapping("/")
     public Response<String> getGreetingMessage() {
-        return new Response<>("success", null, "Hello, World!");
+        return new Response<>("success", null, greetingMessage);
     }
 }
