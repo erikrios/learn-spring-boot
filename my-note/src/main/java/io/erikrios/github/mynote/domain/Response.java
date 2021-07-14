0f2ce6d8b7ae2bc57b6cf2c7c701441d8870c156
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-14 19:12:32 +0700

    Craete Response.java

---
+++ my-note/src/main/java/io/erikrios/github/mynote/domain/Response.java
@@ -0,0 +1,25 @@
+package io.erikrios.github.mynote.domain;
+
+public class Response<T> {
+    private final String status;
+    private final String message;
+    private final T data;
+
+    public Response(String status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public T getData() {
+        return data;
+    }
+}
