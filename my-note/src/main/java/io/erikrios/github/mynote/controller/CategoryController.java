1da67dfe027a8877f17ce4766c82b9ca8e845a97
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-14 20:36:21 +0700

    Create CategoryController.java

---
+++ my-note/src/main/java/io/erikrios/github/mynote/controller/CategoryController.java
@@ -0,0 +1,34 @@
+package io.erikrios.github.mynote.controller;
+
+import io.erikrios.github.mynote.model.request.CreateCategoryRequest;
+import io.erikrios.github.mynote.model.response.CategoryResponse;
+import io.erikrios.github.mynote.model.response.Response;
+import io.erikrios.github.mynote.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class CategoryController {
+
+    private final CategoryService service;
+
+    @Autowired
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @PostMapping(
+            value = "/categories",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Response<CategoryResponse> saveCategory(CreateCategoryRequest request) {
+        CategoryResponse response = service.insert(request);
+        return new Response<>("success", null, response);
+    }
+}
