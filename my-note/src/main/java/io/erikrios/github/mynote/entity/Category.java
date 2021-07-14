22fc6720dca11de6a90b8e0b81ea43553a4fbf30
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-14 19:59:05 +0700

    Create Category.java

---
+++ my-note/src/main/java/io/erikrios/github/mynote/entity/Category.java
@@ -0,0 +1,37 @@
+package io.erikrios.github.mynote.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+public class Category {
+
+    @Id
+    private final String id;
+    @Column(length = 25)
+    private String name;
+
+    public Category(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public Category() {
+        this.id = UUID.randomUUID().toString();
+        this.name = "No Name";
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
