7fb364ae4f8b1cd0255fa28d184dbaca384e21fa
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-25 21:05:34 +0700

    Create Answer.java

--- my-note/src/main/java/io/erikrios/github/mynote/entity/Answer.java
+++ my-note/src/main/java/io/erikrios/github/mynote/entity/Answer.java
@@ -1,6 +1,7 @@
 package io.erikrios.github.mynote.entity;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 @Entity
 public class Answer {
@@ -18,4 +19,42 @@ public class Answer {
     @ManyToOne
     @JoinColumn(name = "question_id")
     private Question question;
+
+    public Answer(String answer, boolean isCorrect, Question question) {
+        id = UUID.randomUUID().toString();
+        this.answer = answer;
+        this.isCorrect = isCorrect;
+        this.question = question;
+    }
+
+    public Answer() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
