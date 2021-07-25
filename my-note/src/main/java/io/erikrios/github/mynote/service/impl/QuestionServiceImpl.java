b31d48f92b2b90c9bb732d400c5dac341773a96b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-07-25 23:26:54 +0700

    Add convertRequestToAnswers and convertAnswersToRequest methods

--- my-note/src/main/java/io/erikrios/github/mynote/service/impl/QuestionServiceImpl.java
+++ my-note/src/main/java/io/erikrios/github/mynote/service/impl/QuestionServiceImpl.java
@@ -8,13 +8,13 @@ import io.erikrios.github.mynote.model.request.CreateAnswerRequest;
 import io.erikrios.github.mynote.model.request.CreateQuestionRequest;
 import io.erikrios.github.mynote.model.response.AnswerResponse;
 import io.erikrios.github.mynote.model.response.QuestionResponse;
+import io.erikrios.github.mynote.repository.AnswerRepository;
 import io.erikrios.github.mynote.repository.CategoryRepository;
 import io.erikrios.github.mynote.repository.QuestionRepository;
 import io.erikrios.github.mynote.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,11 +23,16 @@ public class QuestionServiceImpl implements QuestionService {
 
     private final QuestionRepository questionRepository;
     private final CategoryRepository categoryRepository;
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    public QuestionServiceImpl(QuestionRepository repository, CategoryRepository categoryRepository) {
+    public QuestionServiceImpl(
+            QuestionRepository repository,
+            CategoryRepository categoryRepository,
+            AnswerRepository answerRepository) {
         this.questionRepository = repository;
         this.categoryRepository = categoryRepository;
+        this.answerRepository = answerRepository;
     }
 
     @Override
@@ -45,15 +50,7 @@ public class QuestionServiceImpl implements QuestionService {
 
     private Question convertRequestToQuestion(CreateQuestionRequest request) {
         String question = request.getQuestion();
-        List<CreateAnswerRequest> answerRequests = request.getAnswers();
-
-        List<Answer> answers = new ArrayList<>();
-
-        answerRequests.forEach(answerRequest ->
-                answers.add(new Answer(answerRequest.getAnswer(), answerRequest.isCorrect()))
-        );
-
-        return new Question(question, answers);
+        return new Question(question);
     }
 
     private QuestionResponse convertQuestionToResponse(Question question) {
@@ -65,5 +62,21 @@ public class QuestionServiceImpl implements QuestionService {
         )
                 .collect(Collectors.toList());
         return new QuestionResponse(id, questionName, answerResponses);
+    }
+
+    private List<Answer> convertRequestToAnswers(List<CreateAnswerRequest> requests) {
+        return requests
+                .stream()
+                .map(request ->
+                        new Answer(request.getAnswer(), request.isCorrect())
+                ).collect(Collectors.toList());
+    }
+
+    private List<CreateAnswerRequest> convertAnswersToRequest(List<Answer> answers) {
+        return answers
+                .stream()
+                .map(answer ->
+                        new CreateAnswerRequest(answer.getAnswer(), answer.isCorrect())
+                ).collect(Collectors.toList());
     }
 }
